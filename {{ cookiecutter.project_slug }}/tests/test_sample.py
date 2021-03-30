33941560f36f1f657edecfcae3ca76d371550909
Alexander Frenzel <alex@relatedworks.com>
2021-03-30 12:12:15 +0200

    fix: avoid pytest failing if no tests have been discovered

---
+++ {{ cookiecutter.project_slug }}/tests/test_sample.py
@@ -0,0 +1,8 @@
+def test_placeholder():
+    """
+    This is a placeholder test.
+
+    It avoids that pytest returns the error code 5, if no tests have been
+    discovered.
+    REF: https://github.com/pytest-dev/pytest/issues/2393
+    """
